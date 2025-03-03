f57ecfc358cd32f575dc3318d63a4fd279ec8c03
alvarosalvadorupm <alvaro.salvador@alumnos.upm.es>
2025-03-03 14:55:22 +0100

    Ejercicios 1 y 2 P2 ISE

---
+++ P2/A/HTTP_Server_MCBSTM32F400/HTTP_Server/Thlcd.h
@@ -0,0 +1,21 @@
+#ifndef __THLCD_H
+#define __THLCD_H
+
+	#include "stm32f4xx_hal.h"
+	#include "Driver_SPI.h"
+	#include "stdio.h"
+	#include "string.h"
+	#include "cmsis_os2.h"                          // CMSIS RTOS header file
+	
+	#define S_TRANS_DONE_SPI  0x01
+	
+  void LCD_Init (void);
+  void LCD_Update (void);
+  void LCD_Reset (void);
+  void Init_PinesGPIO (void);
+	void cleanLCD(void);
+	void cleanLine (void);
+	void write_lcd(char cadena[], int linea, int longitudCadena);
+  void pintar_LCD(char cadena[], int linea);
+
+#endif /* __THLCD_H */
